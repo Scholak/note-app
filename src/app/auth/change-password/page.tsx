6bb74075befbdde040e7a6d265e4f9d4d4910a34
scholak <scholaksebahattin@gmail.com>
2023-07-26 20:17:52 +0300

    import error fixed in change-file route

--- src/app/auth/change-password/page.tsx
+++ src/app/auth/change-password/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ChangePasswordForm from '@/components/auth/ChangePasswordForm'
+import { ChangePasswordForm } from '@/components/auth'
 
 export const metadata = {
 	title: 'Change Password'
